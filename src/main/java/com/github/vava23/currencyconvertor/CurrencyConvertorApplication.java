068ef261d0fa06813f96cffbdcf463f1d8b96dd8
vava23 <svv15@mail.ru>
2021-10-28 11:53:07 +0300

    Initial commit (Spring Boot starter)

---
+++ src/main/java/com/github/vava23/currencyconvertor/CurrencyConvertorApplication.java
@@ -0,0 +1,13 @@
+package com.github.vava23.currencyconvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConvertorApplication.class, args);
+	}
+
+}
