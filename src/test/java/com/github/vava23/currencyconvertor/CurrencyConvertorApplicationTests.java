485c844bcf94a60860e65d57fe8af0186788b06a
vava23 <svv15@mail.ru>
2021-11-02 15:38:03 +0300

    Smoke test

--- src/test/java/com/github/vava23/currencyconvertor/CurrencyConvertorApplicationTests.java
+++ src/test/java/com/github/vava23/currencyconvertor/CurrencyConvertorApplicationTests.java
@@ -1,13 +1,25 @@
 package com.github.vava23.currencyconvertor;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.github.vava23.currencyconvertor.rest.MainController;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class CurrencyConvertorApplicationTests {
+	@Autowired
+	MainController mainController;
 
+
+	/** 
+	 * Smoke test
+	 */
 	@Test
 	void contextLoads() {
+		assertNotNull(mainController);
 	}
-
 }
